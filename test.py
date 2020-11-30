5ebcae697b1b402672956c446c062d840b903b90
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2020-11-30 12:29:55 +0800

    covid data csv returned asa string

--- test.py
+++ test.py
@@ -1,6 +1,38 @@
-import sys
+import pandas as pd 
+import requests
+from csv import writer
 
 if __name__ == "__main__":
-    st = sys.argv[1]
-    print(st)
+    page_url = 'https://www.coronatracker.com/analytics/'
+    s = requests.Session()
+    s.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'}
+
+    res = s.get(page_url)
+    res.raise_for_status()
+
+    json_url = 'https://api.coronatracker.com/v3/stats/worldometer/topCountry'
+    try:
+        res = s.get(json_url)
+        res.raise_for_status()
+        data = res.json()
+    except Exception as e:
+        print(e)
+
+
+with open('covid_data.csv', 'w') as csv_file:
+    csv_writer = writer(csv_file)
+    headers = ['country', 'countryCode', 'totalConfirmed', 'totalDeaths', 'totalRecovered', 'dailyConfirmed', 'dailyDeaths', 'activeCases', 'totalCritical', 'lastUpdated']
+    csv_writer.writerow(headers)
+    try:
+        for i in data:
+            csv_writer.writerow(
+                [i['country'], i['countryCode'], i['totalConfirmed'], i['totalDeaths'], i['totalRecovered'],
+                 i['dailyConfirmed'], i['dailyDeaths'], i['activeCases'], i['totalCritical'], i['lastUpdated']])
+    except Exception as e:
+        print(e)
+
+data2 = pd.read_csv("covid_data.csv")
+data2.head()
+print(data2)
+
 
